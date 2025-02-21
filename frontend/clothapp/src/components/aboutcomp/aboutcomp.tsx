b6a4fd6adc32b10595a1673193a5b4b42c42f2cd
Blessen Vinoy Mathew <blessenvinoymathew25@gmail.com>
2025-02-21 21:23:07 +0530

    Commited Changes

--- frontend/clothapp/src/components/aboutcomp/aboutcomp.tsx
+++ frontend/clothapp/src/components/aboutcomp/aboutcomp.tsx
@@ -10,7 +10,16 @@ export const AboutIntroComp = () =>{
             <div className="maindivcstmeverypage aboutusintodiv">
                 <h1 className="mainheadingcstm">About Us</h1>
                 <FlexCompwithimg img={"assets/Images/models_suit_men.jpg"} htext={"QUALITY MEETS STYLE"} ptext={"At Shop.Cloth, we believe that fashion is more than just clothing—it's a statement of individuality, confidence, and self-expression. Our carefully curated collections are designed to bring you the latest trends while maintaining an emphasis on quality and affordability. From timeless classics to bold modern styles, we ensure that every piece is crafted with premium fabrics and attention to detail, so you can look and feel your best—every day."} button={true} buttonlabel="View All"/>
+                <Aboutusthreefeatures />
             </div>
         </>
+    )
+}
+
+export const Aboutusthreefeatures = () => {
+    return(
+        <div className="aboutusthreefeaturesdiv">
+            
+        </div>
     )
 }
