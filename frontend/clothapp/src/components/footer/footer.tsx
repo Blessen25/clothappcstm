160d4e1af0a020be6f1371b9b204afd856e373f9
Blessen25 <blessenvinoymathew25@gmail.com>
2025-02-24 14:09:55 +0530

    Commited Changes

--- frontend/clothapp/src/components/footer/footer.tsx
+++ frontend/clothapp/src/components/footer/footer.tsx
@@ -9,7 +9,7 @@ export const Footer = () => {
         e.preventDefault();
         console.log("Submit Value", inputform);
     }
-    return(
+    return (
         <>
             <div className="footerparentdiv">
                 <div className="stayuptodatediv">
@@ -17,68 +17,69 @@ export const Footer = () => {
                         <h1>STAY UPTO DATE ABOUT OUR LATEST OFFERS</h1>
                     </div>
                     <form action="#" onSubmit={handleSubmit} className="stayemailandsubmitdiv">
-                            <div className="stayemaildiv">
-                                <input type="email" placeholder="Enter Your Email Address ..." value={inputform} onChange={(e) => setInputForm(e.target.value)} className="stayemailinput"/>
-                                <FontAwesomeIcon icon={faEnvelope} className="envelopeicon"/>
-                            </div>
-                            <button type="submit" className="staysubmitbutton" >Subscribe</button> 
+                        <div className="stayemaildiv">
+                            <input type="email" placeholder="Enter Your Email Address ..." value={inputform} onChange={(e) => setInputForm(e.target.value)} className="stayemailinput" />
+                            <FontAwesomeIcon icon={faEnvelope} className="envelopeicon" />
+                        </div>
+                        <button type="submit" className="staysubmitbutton" >Subscribe</button>
                     </form>
                 </div>
                 <div className="footerbottomdiv">
-                     <div className="footerbottomchildone">
+                    <div className="footerbottomchildone">
                         <h1>SHOP.CLOTH</h1>
                         <p>We have clothes that suits your style and which you’re proud to wear. From women to men.</p>
                         <div className="socialmedialogos">
                             <div className="twitterdiv">
                                 <a href="#">
-                                    <img src="assets/Logo/Twittterround.png" alt="Twitter Round" className="twitterroundimg"/>
-                                    <img src="assets/Logo/Twitter.png" alt="Twitter" className="twitterlogoimg"/>
+                                    <img src="assets/Logo/Twittterround.png" alt="Twitter Round" className="twitterroundimg" />
+                                    <img src="assets/Logo/Twitter.png" alt="Twitter" className="twitterlogoimg" />
                                 </a>
                             </div>
                             <a href="#">
-                                <img src="assets/Logo/Facebooklogo.png" alt="Facebook" className="facebooklogoimg"/>
+                                <img src="assets/Logo/Facebooklogo.png" alt="Facebook" className="facebooklogoimg" />
                             </a>
                             <a href="#">
-                                <img src="assets/Logo/Instagramlogo.png" alt="Instagram" className="instalogoimg"/>
+                                <img src="assets/Logo/Instagramlogo.png" alt="Instagram" className="instalogoimg" />
                             </a>
                             <a href="#">
                                 <img src="assets/Logo/Githublogo.png" alt="Github" className="githublogoimg" />
                             </a>
                         </div>
-                     </div>
-                     <div className="footerbottomchildtwo">
+                    </div>
+                    <div className="footerbottomchildtwo">
                         <p>Pages</p>
                         <a href="/">Home</a>
                         <a href="/Aboutus">About</a>
+                        <a href="#">Contact</a>
                         <a href="#">Product</a>
                         <a href="#">Category</a>
-                     </div>
-                     <div className="footerbottomchildtwo">
+                    </div>
+                    <div className="footerbottomchildtwo">
                         <p>HELP</p>
                         <a href="#">Customer Support</a>
                         <a href="#">Delivery Details</a>
                         <a href="#">Terms & Conditions</a>
                         <a href="#">Privacy Policy</a>
-                        
-                     </div>
-                     <div className="footerbottomchildtwo">
+
+                    </div>
+                    <div className="footerbottomchildtwo">
                         <p>FAQ</p>
                         <span><a href="#">Account</a></span>
                         <a href="#">Manage Deliveries</a>
                         <a href="#">Orders</a>
                         <a href="#">Payments</a>
-                     </div>
-                     <div className="footerbottomchildtwo">
+                    </div>
+                    <div className="footerbottomchildtwo">
                         <p>RESOURCES</p>
                         <a href="#">Free eBooks</a>
                         <a href="#">Development Tutorial</a>
                         <a href="#">How to - Blog</a>
                         <a href="#">Youtube Playlist</a>
-                     </div>
+                    </div>
                 </div>
-                <hr className="footerhrcstm"/>
+                <hr className="footerhrcstm" />
                 <div className="footercopyrightdiv">
-                    <p>Shop.Cloth © 2025, All Rights Reserved</p>
+                    <p>© 2025 Shop.Cloth</p>
                     {/* <div className="footerpaymentcards">
                         <img src="assets/Logo/Visalogo.png" alt="" style={{ objectFit:'cover'}}/>
                         <img src="assets/Logo/googlepaylogo.png" alt="" style={{ objectFit:'cover'}} />
--- frontend/clothapp/src/components/header/header.tsx
+++ frontend/clothapp/src/components/header/header.tsx
@@ -5,7 +5,7 @@ import { Search, ShoppingCart, } from "react-feather";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
-const Header = () =>{
+const Header = () => {
 
     const handleSidebarChange = () => {
         setSideBarActive(!isSideBarActive)
@@ -14,70 +14,72 @@ const Header = () =>{
 
     const [isSideBarActive, setSideBarActive] = useState(false)
     const [isSearchBarActive, setIsSearchBarActive] = useState(false)
-    const handleSearchBarChange = () =>{
+    const handleSearchBarChange = () => {
         setIsSearchBarActive(!isSearchBarActive)
     }
-    return(
+    return (
         <>
-        <section id="Header">
-            <div className="headerparent">
-                <div className="headerchildfirst">
-                    <h1>Shop.Cloth</h1>
-                </div>
-                <div className="headerchildsecond">
-                    <a href="/" >Home</a>
-                    <a href="/Aboutus">About</a>
-                    <a href="#">Product</a>
-                    <a href="#">Category</a>
-                </div>
-                <div className="headerchildfourth">
-                    <div className="headerchildthird">
-                        <input type="Search" className="searchbar" placeholder="Search For Products..."/>
-                        <Search className="searchiconcstmthird"/>
+            <section id="Header">
+                <div className="headerparent">
+                    <div className="headerchildfirst">
+                        <h1>Shop.Cloth</h1>
                     </div>
-                    <a href="#"><ShoppingCart className="headericonclass"/></a>
-                    <a href="#"><FaRegUserCircle className="headericonclass"/></a>
-                </div>
-                <div className="headerrespcontainer-cstmone">
-                    <FaBars className="fabarsicon" onClick={handleSidebarChange}/>
-                    <h1 className="h1textcstm">Shop.Cloth</h1>
-                </div>
-                {isSearchBarActive ? (
-                    <>
-                    <div className="headerrespcontainer-cstmtwo">
-                        <div className="searchbariconparentcstm">
-                            <input type="search" className="searchbarcstm" placeholder="Search For Products..."/>
-                            <Search className="headericonclass searchbariconcstm" onClick={handleSearchBarChange}/>
+                    <div className="headerchildsecond">
+                        <a href="/" >Home</a>
+                        <a href="/Aboutus">About</a>
+                        <a href="#">Contact</a>
+                        <a href="#">Product</a>
+                        <a href="#">Category</a>
+                    </div>
+                    <div className="headerchildfourth">
+                        <div className="headerchildthird">
+                            <input type="Search" className="searchbar" placeholder="Search For Products..." />
+                            <Search className="searchiconcstmthird" />
                         </div>
-                        <a href="#"><ShoppingCart className="headericonclass"/></a>
-                        <a href="#"><FaRegUserCircle className="headericonclass"/></a>
+                        <a href="#"><ShoppingCart className="headericonclass" /></a>
+                        <a href="#"><FaRegUserCircle className="headericonclass" /></a>
                     </div>
-                    </>
-                ) : (
-                <>
-                    <div className="headerrespcontainer-cstmsecond">
-                        <Search className="headericonclass searchbariconcustom" onClick={handleSearchBarChange}/>
-                        <a href="#"><ShoppingCart className="headericonclass"/></a>
-                        <a href="#"><FaRegUserCircle className="headericonclass"/></a>
+                    <div className="headerrespcontainer-cstmone">
+                        <FaBars className="fabarsicon" onClick={handleSidebarChange} />
+                        <h1 className="h1textcstm">Shop.Cloth</h1>
                     </div>
-                </>
-            ) }
-            </div>
-        </section>
-        {isSideBarActive && (<> 
-            <div className="outlaysidebar"></div>
-            <div className="sidebar">
-                <div className="searchbardivsidebarcstm">
-                    <input type="Search" placeholder="Search For Products..." className="cstmsearchsidebar"/>
-                    <FontAwesomeIcon icon={faSearch} className="searchiconsidebarcstm"/>
+                    {isSearchBarActive ? (
+                        <>
+                            <div className="headerrespcontainer-cstmtwo">
+                                <div className="searchbariconparentcstm">
+                                    <input type="search" className="searchbarcstm" placeholder="Search For Products..." />
+                                    <Search className="headericonclass searchbariconcstm" onClick={handleSearchBarChange} />
+                                </div>
+                                <a href="#"><ShoppingCart className="headericonclass" /></a>
+                                <a href="#"><FaRegUserCircle className="headericonclass" /></a>
+                            </div>
+                        </>
+                    ) : (
+                        <>
+                            <div className="headerrespcontainer-cstmsecond">
+                                <Search className="headericonclass searchbariconcustom" onClick={handleSearchBarChange} />
+                                <a href="#"><ShoppingCart className="headericonclass" /></a>
+                                <a href="#"><FaRegUserCircle className="headericonclass" /></a>
+                            </div>
+                        </>
+                    )}
                 </div>
-                <a href="/" onClick={handleSidebarChange}>Home</a>
-                <a href="/Aboutus" onClick={handleSidebarChange}>About</a>
-                <a href="#" onClick={handleSidebarChange}>Product</a>
-                <a href="#" onClick={handleSidebarChange}>Category</a>
-            </div>
-        </>)}
-        
+            </section>
+            {isSideBarActive && (<>
+                <div className="outlaysidebar"></div>
+                <div className="sidebar">
+                    <div className="searchbardivsidebarcstm">
+                        <input type="Search" placeholder="Search For Products..." className="cstmsearchsidebar" />
+                        <FontAwesomeIcon icon={faSearch} className="searchiconsidebarcstm" />
+                    </div>
+                    <a href="/" onClick={handleSidebarChange}>Home</a>
+                    <a href="/Aboutus" onClick={handleSidebarChange}>About</a>
+                    <a href="#" onClick={handleSidebarChange}>Contact</a>
+                    <a href="#" onClick={handleSidebarChange}>Product</a>
+                    <a href="#" onClick={handleSidebarChange}>Category</a>
+                </div>
+            </>)}
+
         </>
     )
 }
