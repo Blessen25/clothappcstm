014075c49781f6d883501b70b76acddd6490e2d4
Blessen25 <blessenvinoymathew25@gmail.com>
2025-02-25 15:55:50 +0530

    Commited Changes

--- frontend/clothapp/src/App.tsx
+++ frontend/clothapp/src/App.tsx
@@ -7,36 +7,33 @@ import Homefrontband from './components/headerbandsale.tsx';
 import { Footer } from './components/footer/footer.tsx';
 import AboutusPage from './screens/about/about.tsx';
 import ContactPage from './screens/contact/contact.tsx';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
 
-
-const App = () =>{
-  return(
+const App = () => {
+  return (
     <>
       <Router>
         <Routes>
           <Route path='/' element={
-          <>
-            <Homefrontband/>
-            <Header/>
-            <Home/>
-            <Footer />
-          </>
-        }/>
-        <Route path='/Aboutus' element={
-          <>
-            <Header />
-            <AboutusPage />
-            <Footer />
-          </>
-        } />
-        <Route path='/Contact' element={
-          <>
-            <Header />
-            <ContactPage />
-          </>
-        } />
+            <>
+              <Homefrontband />
+              <Header />
+              <Home />
+              <Footer />
+            </>
+          } />
+          <Route path='/Aboutus' element={
+            <>
+              <Header />
+              <AboutusPage />
+              <Footer />
+            </>
+          } />
+          <Route path='/Contact' element={
+            <>
+              <Header />
+              <ContactPage />
+            </>
+          } />
         </Routes>
       </Router>
     </>
