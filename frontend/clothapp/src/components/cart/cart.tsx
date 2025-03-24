b8e3d7b3c82f76ff45805a465aafc12da0981161
Blessen25 <blessenvinoymathew25@gmail.com>
2025-03-24 10:33:53 +0530

    Commited Changes

--- frontend/clothapp/src/components/cart/cart.tsx
+++ frontend/clothapp/src/components/cart/cart.tsx
@@ -8,7 +8,7 @@ import { ItemscartdivProps } from "../../interface/interface";
 
 const cardData = [
     {
-        id : 1,
+        id: 1,
         image: "assets/Images/browndress.png",
         title: "ONE LIFE GRAPHIC T-SHIRT",
         size: "Large",
@@ -16,7 +16,7 @@ const cardData = [
         price: 220,
     },
     {
-        id : 2,
+        id: 2,
         image: "assets/Images/browndress.png",
         title: "ONE LIFE GRAPHIC T-SHIRT",
         size: "Small",
@@ -24,7 +24,7 @@ const cardData = [
         price: 240,
     },
     {
-        id : 3,
+        id: 3,
         image: "assets/Images/browndress.png",
         title: "ONE LIFE GRAPHIC T-SHIRT",
         size: "Medium",
@@ -32,7 +32,7 @@ const cardData = [
         price: 260,
     },
     {
-        id : 4,
+        id: 4,
         image: "assets/Images/browndress.png",
         title: "ONE LIFE GRAPHIC T-SHIRT",
         size: "Large",
@@ -46,134 +46,144 @@ const cardData = [
 
 const AddtoCart = () => {
 
-    const [activatecoupen, setActivateCoupen ] = useState(false)
+    const [activatecoupen, setActivateCoupen] = useState(false)
     const [cartItems, setCartItems] = useState(cardData);
+    const [modalopen, setModalOpen] = useState(false);
 
     const handleActivateCoupen = () => {
 
         setActivateCoupen(!activatecoupen)
     }
 
-    const handleDeletedItem = (index:number) => {
+    const handleDeletedItem = (index: number) => {
 
         setCartItems(prevItems => prevItems.filter((_, i) => i !== index));
     }
 
-    console.log("Cart items",cartItems)
+    console.log("Cart items", cartItems)
     return (
         <>
-            <div className="addtocartintro">
-                <h1 className="mainheadingcstm">YOUR CART</h1>
-                <div className="cartamountanditemparent">
-                    <div className="itemschild">
-                        <div className="itemcartdiv">
-                        {cartItems.length === 0 ? (
-                            <> 
+            <div className="containerdivcstm">
+                <div className="addtocartintro">
+                    <h1 className="mainheadingcstm">YOUR CART</h1>
+                    <div className="cartamountanditemparent">
+                        <div className="itemschild">
+                            <div className="itemcartdiv">
+                                {cartItems.length === 0 ? (
+                                    <>
 
-                                <h1 className="additemstocarthead">Please Add Items to the Cart</h1>
-                            </>
-                        ) : (
+                                        <h1 className="additemstocarthead">Please Add Items to the Cart</h1>
+                                    </>
+                                ) : (
+                                    <>
+
+                                        {
+                                            cartItems.map((card, index) => (
+                                                <React.Fragment key={card.id}>
+                                                    {index > 0 && <div className="divider-cstm"></div>}
+                                                    <Itemscartdiv
+                                                        id={card.id}
+                                                        image={card.image}
+                                                        title={card.title}
+                                                        size={card.size}
+                                                        quantity={card.quantity}
+                                                        price={card.price}
+                                                        hasDeleted={() => handleDeletedItem(index)}
+
+                                                    />
+                                                </React.Fragment>
+                                            ))}
+                                    </>
+                                )}
+                            </div>
+                        </div>
+                        {cartItems.length === 0 ? (
                             <>
 
-                            {
-                                cartItems.map((card, index) => (
-                                    <React.Fragment key={card.id}>
-                                        {index > 0 && <div className="divider-cstm"></div>}
-                                        <Itemscartdiv
-                                            id={card.id}
-                                            image={card.image}
-                                            title={card.title}
-                                            size={card.size}
-                                            quantity={card.quantity}
-                                            price={card.price}
-                                            hasDeleted = {() => handleDeletedItem(index)}
-                                            
-                                        />
-                                    </React.Fragment>
-                                ))}
-                            </>
-                        ) }
-                        </div>
-                    </div>
-                    {cartItems.length === 0 ? (
-                        <>
+                                <div className="amountchild">
+                                    <div className="amountdiv">
+                                        <h1>Order Summary</h1>
+                                        <div className="totalsubtotalcstmdiv">
+                                            <div className="subtitlecstmdiv">
+                                                <p>Subtotal</p>
+                                                <h2>Rs 0</h2>
+                                            </div>
+                                            <div className="subtitlecstmdiv">
+                                                <p>Discount (-20%)</p>
+                                                <h2>Rs 0</h2>
+                                            </div>
+                                            <div className="subtitlecstmdiv">
+                                                <p>Delivery Fee</p>
+                                                <h2>Rs 0</h2>
+                                            </div>
+                                            <div className="border-cstm"></div>
+                                            <div className="subtitlecstmdiv">
+                                                <h1>Total Price</h1>
+                                                <h3>Rs 0</h3>
+                                            </div>
+                                            <div className="border-cstm"></div>
 
-<div className="amountchild">
-                        <div className="amountdiv">
-                            <h1>Order Summary</h1>
-                            <div className="totalsubtotalcstmdiv">
-                                    <div className="subtitlecstmdiv">
-                                        <p>Subtotal</p>
-                                        <h2>Rs 0</h2>
+                                        </div>
+                                        {!activatecoupen && (
+                                            <a href="#" className="buttonaddtocartcstm buttonaddtocartcstm1" onClick={handleActivateCoupen}>Add Discount Coupen</a>
+                                        )}
+                                        {activatecoupen && (
+                                            <div className="activatecoupen">
+                                                <input type="text" className="activatecoupentextinput" placeholder="Add Coupen Code" />
+                                                <a href="#" className="buttonactivatecoupencstm">Add Discount</a>
+                                            </div>
+                                        )}
+                                        <a href="#" className="buttonaddtocartcstm buttonaddtocartcstm2">Buy Now</a>
                                     </div>
-                                    <div className="subtitlecstmdiv">
-                                        <p>Discount (-20%)</p>
-                                        <h2>Rs 0</h2>
-                                    </div>
-                                    <div className="subtitlecstmdiv">
-                                        <p>Delivery Fee</p>
-                                        <h2>Rs 0</h2>
-                                    </div>
-                                    <div className="border-cstm"></div>
-                                    <div className="subtitlecstmdiv">
-                                        <h1>Total Price</h1>
-                                        <h3>Rs 0</h3>
-                                    </div>
-                                    <div className="border-cstm"></div>
-                                    
-                            </div>
-                            {!activatecoupen && (
-                                <a href="#" className="buttonaddtocartcstm buttonaddtocartcstm1" onClick={handleActivateCoupen}>Add Discount Coupen</a>
-                            )}
-                            {activatecoupen && (
-                                <div className="activatecoupen">
-                                <input type="text" className="activatecoupentextinput" placeholder="Add Coupen Code"/>
-                                <a href="#" className="buttonactivatecoupencstm">Add Discount</a>
                                 </div>
-                            )}
-                            <a href="#" className="buttonaddtocartcstm buttonaddtocartcstm2">Buy Now</a>
-                        </div>
-                    </div>
-                        </>
-                    ) : (<>
+                            </>
+                        ) : (<>
 
                             <div className="amountchild">
-                        <div className="amountdiv">
-                            <h1>Order Summary</h1>
-                            <div className="totalsubtotalcstmdiv">
-                                    <div className="subtitlecstmdiv">
-                                        <p>Subtotal</p>
-                                        <h2>Rs 600</h2>
+                                <div className="amountdiv">
+                                    <h1>Order Summary</h1>
+                                    <div className="totalsubtotalcstmdiv">
+                                        <div className="subtitlecstmdiv">
+                                            <p>Subtotal</p>
+                                            <h2>Rs 600</h2>
+                                        </div>
+                                        <div className="subtitlecstmdiv">
+                                            <p>Discount (-20%)</p>
+                                            <h2>Rs 600</h2>
+                                        </div>
+                                        <div className="subtitlecstmdiv">
+                                            <p>Delivery Fee</p>
+                                            <h2>Rs 600</h2>
+                                        </div>
+                                        <div className="border-cstm"></div>
+                                        <div className="subtitlecstmdiv">
+                                            <h1>Total Price</h1>
+                                            <h3>Rs 1800</h3>
+                                        </div>
+                                        <div className="border-cstm"></div>
+
                                     </div>
-                                    <div className="subtitlecstmdiv">
-                                        <p>Discount (-20%)</p>
-                                        <h2>Rs 600</h2>
-                                    </div>
-                                    <div className="subtitlecstmdiv">
-                                        <p>Delivery Fee</p>
-                                        <h2>Rs 600</h2>
-                                    </div>
-                                    <div className="border-cstm"></div>
-                                    <div className="subtitlecstmdiv">
-                                        <h1>Total Price</h1>
-                                        <h3>Rs 1800</h3>
-                                    </div>
-                                    <div className="border-cstm"></div>
-                                    
-                            </div>
-                            {!activatecoupen && (
-                                <a href="#" className="buttonaddtocartcstm buttonaddtocartcstm1" onClick={handleActivateCoupen}>Add Discount Coupen</a>
-                            )}
-                            {activatecoupen && (
-                                <div className="activatecoupen">
-                                <input type="text" className="activatecoupentextinput" placeholder="Add Coupen Code"/>
-                                <a href="#" className="buttonactivatecoupencstm">Add Discount</a>
+                                    {!activatecoupen && (
+                                        <a href="#" className="buttonaddtocartcstm buttonaddtocartcstm1" onClick={handleActivateCoupen}>Add Discount Coupen</a>
+                                    )}
+                                    {activatecoupen && (
+                                        <div className="activatecoupen">
+                                            <input type="text" className="activatecoupentextinput" placeholder="Add Coupen Code" />
+                                            <a href="#" className="buttonactivatecoupencstm">Add Discount</a>
+                                        </div>
+                                    )}
+                                    <a href="#" className="buttonaddtocartcstm buttonaddtocartcstm2">Buy Now</a>
                                 </div>
-                            )}
-                            <a href="#" className="buttonaddtocartcstm buttonaddtocartcstm2">Buy Now</a>
+                            </div>
+                        </>)}
+                    </div>
+                </div>
+                <div className="modalparentdivcstm">
+                    <div className="modalmaindiv">
+                        <div className="modalimgdiv">
+
                         </div>
                     </div>
-                    </>)}
                 </div>
             </div>
         </>
@@ -201,25 +211,25 @@ const Itemscartdiv: React.FC<ItemscartdivProps> = (Props) => {
 
         <>
             <div className="itemdivparent">
-            <div className="imagedivparent">
-                <img src={Props.image} alt="" className="imagecstmchils" />
-            </div>
-            <div className="contentdivparent">
-                <div className="nameanddeletebutton"> 
-                <h1>{Props.title}</h1>
-                <FontAwesomeIcon icon={faTrash} className="trashcancstm" onClick={Props.hasDeleted}/>
+                <div className="imagedivparent">
+                    <img src={Props.image} alt="" className="imagecstmchils" />
                 </div>
-                <p>Size : {Props.size}</p>
-                <p>Quantity : {quantity}</p>
-                <div className="priceandquantitydiv">
-                    <h1>RS {Props.price}</h1>
-                    <div className="quantitydivbtncstm">
-                        <FontAwesomeIcon icon={faMinus} className="faiconcartdetails" onClick={handlequantityminus} />
-                        <p>{quantity}</p>
-                        <FontAwesomeIcon icon={faPlus} className="faiconcartdetails" onClick={handlequantityplus} />
+                <div className="contentdivparent">
+                    <div className="nameanddeletebutton">
+                        <h1>{Props.title}</h1>
+                        <FontAwesomeIcon icon={faTrash} className="trashcancstm" onClick={Props.hasDeleted} />
+                    </div>
+                    <p>Size : {Props.size}</p>
+                    <p>Quantity : {quantity}</p>
+                    <div className="priceandquantitydiv">
+                        <h1>RS {Props.price}</h1>
+                        <div className="quantitydivbtncstm">
+                            <FontAwesomeIcon icon={faMinus} className="faiconcartdetails" onClick={handlequantityminus} />
+                            <p>{quantity}</p>
+                            <FontAwesomeIcon icon={faPlus} className="faiconcartdetails" onClick={handlequantityplus} />
+                        </div>
                     </div>
                 </div>
-            </div>
             </div>
         </>
     )
